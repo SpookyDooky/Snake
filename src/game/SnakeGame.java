78eee00780f4c79db76692100382a645ac078dfa
Raymond <r.danjou@student.tudelft.nl>
2019-04-22 15:57:06 +0200

    Adding the launcher

---
+++ src/game/SnakeGame.java
@@ -0,0 +1,12 @@
+package game;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class SnakeGame extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+}
---
+++ src/game/menus/MainMenu.java
@@ -0,0 +1,10 @@
+package game.menus;
+
+import javafx.scene.layout.Pane;
+
+public class MainMenu extends MenuScene{
+
+    public MainMenu(Pane rootpane){
+        super(rootpane);
+    }
+}
