f50ffc2b4ca4ee59fe7544acb58e2541ac8ab608
Raymond <r.danjou@student.tudelft.nl>
2019-04-22 12:54:27 +0200

    Finished most of the units, might need to add more later in the future.

---
+++ src/game/unit/movingunits/SnakeBody.java
@@ -0,0 +1,23 @@
+package game.unit.movingunits;
+
+import game.Direction;
+import game.unit.MovingUnit;
+
+public class SnakeBody extends MovingUnit {
+
+    private MovingUnit nextChainLink;
+
+    public SnakeBody(int x, int y, Direction direction, MovingUnit next){
+        super(x,y,direction);
+        this.nextChainLink = next;
+    }
+
+    /**
+     * Updates the current direction, to the one in front of this body part, so that it becomes a train like moving unit.
+     */
+    public void updateDirection(){
+        if(this.nextChainLink.getDirection() != this.getDirection()){
+            this.setDirection(this.nextChainLink.getDirection());
+        }
+    }
+}
