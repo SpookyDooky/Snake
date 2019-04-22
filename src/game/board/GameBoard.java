06a1b279f041db54fa461976f220d75582c2d9d6
Raymond <r.danjou@student.tudelft.nl>
2019-04-22 13:22:22 +0200

    Working on the actual board class

--- src/game/board/GameBoard.java
+++ src/game/board/GameBoard.java
@@ -5,15 +5,31 @@ public class GameBoard {
     private int width;
     private int height;
 
+    private Coordinate spawnpoint;
     private Tile[][] tileGrid;
 
-    public GameBoard(int width, int height){
+    public GameBoard(int width, int height, Coordinate spawnpoint){
         this.width = width;
         this.height = height;
+        this.spawnpoint = spawnpoint;
         this.tileGrid = new Tile[width][height];
     }
 
-    public void initBoard(){
-
+    public int getWidth(){
+        return this.width;
     }
+
+    public int getHeight(){
+        return this.height;
+    }
+
+    public void initBoard(){
+        for(int x = 0; x < this.width;x++){
+            for(int y = 0; y < this.height;y++){
+
+            }
+        }
+    }
+
+
 }
