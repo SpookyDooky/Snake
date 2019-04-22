3506d4fa92ea630669de0574bf982c2e05ed0eaf
Raymond <r.danjou@student.tudelft.nl>
2019-04-22 16:03:14 +0200

    Can now close the game

--- src/game/SnakeGame.java
+++ src/game/SnakeGame.java
@@ -2,6 +2,7 @@ package game;
 
 import game.menus.MainMenu;
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
@@ -16,12 +17,16 @@ public class SnakeGame extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         this.mainStage = primaryStage;
-        primaryStage.setTitle("Snake by SpookyBuster");
+        primaryStage.setTitle("Snake");
 
         this.mainScene = new Scene(new Pane(),1000,1000);
         primaryStage.show();
 
         launchMenu();
+        primaryStage.setOnCloseRequest(event -> {
+            System.out.println("Shutting down game...");
+            Platform.exit();
+        });
     }
 
     private void launchMenu(){
