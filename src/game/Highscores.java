95a04573548073bf3cb38002a863b7a3ec20ac4b
Raymond <r.danjou@student.tudelft.nl>
2019-04-25 21:11:45 +0200

    Added more functionalities to highscores class

--- src/game/Highscores.java
+++ src/game/Highscores.java
@@ -1,5 +1,6 @@
 package game;
 
+import java.io.*;
 import java.util.ArrayList;
 
 public class Highscores {
@@ -11,7 +12,31 @@ public class Highscores {
     }
 
     //For now we will only have highscores for one map, but in the future this needs to be compatible with multiple maps.
-    public void init(){
 
+    /**
+     * For initializing the highscores
+     */
+    public void init(){
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(new File("src" + File.separator + "highscores" + File.separator + "highscore")));
+            int amount = Integer.valueOf(reader.readLine());
+
+            for(int x = 0; x < amount;x++){
+                String line = reader.readLine();
+                String[] splitted = line.split(" ");
+
+                HighscoreEntry entry = new HighscoreEntry(splitted[0],Integer.valueOf(splitted[1]));
+                this.highscores.add(entry);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void addHighscore(String name, int score){
+        HighscoreEntry newEntry = new HighscoreEntry(name,score);
+        this.highscores.add(newEntry);
     }
 }
