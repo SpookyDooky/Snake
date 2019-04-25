f55c369f18beba5411b3e932ce00483ae798f259
hypothermic <admin@hypothermic.nl>
2019-04-25 16:28:57 +0200

    Add average fps counter
    Signed-off-by: hypothermic <admin@hypothermic.nl>

--- src/game/Profiler.java
+++ src/game/Profiler.java
@@ -1,6 +1,10 @@
 package game;
 
+import java.util.Arrays;
+
 public class Profiler {
+
+    public static final int FPS_BUFFER_SIZE = 10;
 
     private static final Profiler instance = new Profiler();
 
@@ -10,18 +14,28 @@ public class Profiler {
 
 
     public long nanos;
-    public volatile long delta = 1;
+    public long delta = 1;
+    public long fps = 1;
+
+    public int fpsBufferPos = 0;
+    public final long[] fpsBuffer = new long[FPS_BUFFER_SIZE];
 
     public void begin() {
         nanos = System.nanoTime();
     }
 
     public void end() {
+        fpsBufferPos += 1;
+        if (fpsBufferPos > FPS_BUFFER_SIZE - 1) {
+            fpsBufferPos = 0;
+        }
         delta = System.nanoTime() - nanos;
+        fps = (100000000 / delta);
+        fpsBuffer[fpsBufferPos] = fps;
     }
 
     @Override
     public String toString() {
-        return delta + " ns, " + (100000000 / delta) + " fps";
+        return delta + " ns, " + (100000000 / delta) + " fps (avg " + Arrays.stream(fpsBuffer).average().getAsDouble() + ")";
     }
 }
