ba05e58bfd44a3f9e7e3e271b12c546e78e83c7a
Raymond <r.danjou@student.tudelft.nl>
2019-04-26 14:47:23 +0200

    Finished the gameover screen

--- src/game/menus/GameOver.java
+++ src/game/menus/GameOver.java
@@ -1,5 +1,7 @@
 package game.menus;
 
+import game.HighscoreEntry;
+import game.Highscores;
 import game.SnakeGame;
 import game.gamelogic.State;
 import javafx.scene.control.Button;
@@ -8,6 +10,8 @@ import javafx.scene.layout.Pane;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.scene.text.TextAlignment;
+
+import java.util.ArrayList;
 
 public class GameOver extends MenuScene {
 
@@ -18,6 +22,9 @@ public class GameOver extends MenuScene {
         setupScoreText();
         setupEnterName();
         setupInputField();
+        setupSubmitButton();
+        setupHeaders();
+        setupScores();
     }
 
 
@@ -80,15 +87,109 @@ public class GameOver extends MenuScene {
 
         submitButton.setText("Submit");
         submitButton.setFont(this.getMenuFont());
-        
+
+        submitButton.setMinWidth(150);
+        submitButton.setMaxWidth(150);
+        submitButton.setMinHeight(50);
+        submitButton.setMaxHeight(50);
+
+        submitButton.setLayoutX(575);
+        submitButton.setLayoutY(175);
+
+        this.getRootPane().getChildren().add(submitButton);
     }
 
     private void setupHeaders(){
+        Text header1 = new Text();
+        Text header2 = new Text();
+        Text header3 = new Text();
 
+        header1.setText("Rank");
+        header2.setText("Name");
+        header3.setText("Score");
+
+        header1.setWrappingWidth(250);
+        header2.setWrappingWidth(300);
+        header3.setWrappingWidth(250);
+
+        header1.setTextAlignment(TextAlignment.CENTER);
+        header2.setTextAlignment(TextAlignment.CENTER);
+        header3.setTextAlignment(TextAlignment.CENTER);
+
+        header1.setFont(this.getMenuFont());
+        header2.setFont(this.getMenuFont());
+        header3.setFont(this.getMenuFont());
+
+        header1.setLayoutY(275);
+        header2.setLayoutY(275);
+        header3.setLayoutY(275);
+
+        header2.setLayoutX(250);
+        header3.setLayoutX(550);
+
+        this.getRootPane().getChildren().add(header1);
+        this.getRootPane().getChildren().add(header2);
+        this.getRootPane().getChildren().add(header3);
     }
 
-    private void setupHighscores(){
+    private void setupScores(){
+        Text highscoreRank = new Text();
+        Text highscoreName = new Text();
+        Text highscoreScore = new Text();
 
+        SnakeGame.getGameInstance().initHighscores();
+        Highscores currentScores = SnakeGame.getGameInstance().getHighscores();
+
+        ArrayList<HighscoreEntry> entries = currentScores.getEntries();
+        String rankString = "";
+        String nameString = "";
+        String scoreString = "";
+
+        int amount = 10;
+        int fillerBuff = 0;
+        if(entries.size() < 10){
+            amount = entries.size();
+            fillerBuff = 10 - amount;
+        }
+
+        for(int x = 1; x <= amount;x++){
+            HighscoreEntry currentEntry = entries.get(x-1);
+            rankString += String.valueOf(x) + "\n";
+            nameString += currentEntry.getName() + "\n";
+            scoreString += String.valueOf(currentEntry.getScore()) + "\n";
+        }
+
+        for(int x = 1; x <= fillerBuff;x++){
+            int spot = amount + x;
+            rankString += String.valueOf(spot) + "\n";
+        }
+
+        highscoreRank.setText(rankString);
+        highscoreName.setText(nameString);
+        highscoreScore.setText(scoreString);
+
+        highscoreRank.setWrappingWidth(250);
+        highscoreName.setWrappingWidth(300);
+        highscoreScore.setWrappingWidth(250);
+
+        highscoreRank.setTextAlignment(TextAlignment.CENTER);
+        highscoreName.setTextAlignment(TextAlignment.CENTER);
+        highscoreScore.setTextAlignment(TextAlignment.CENTER);
+
+        highscoreRank.setFont(this.getMenuFont());
+        highscoreName.setFont(this.getMenuFont());
+        highscoreScore.setFont(this.getMenuFont());
+
+        highscoreRank.setLayoutY(300);
+        highscoreName.setLayoutY(300);
+        highscoreScore.setLayoutY(300);
+
+        highscoreName.setLayoutX(250);
+        highscoreScore.setLayoutX(550);
+
+        this.getRootPane().getChildren().add(highscoreRank);
+        this.getRootPane().getChildren().add(highscoreName);
+        this.getRootPane().getChildren().add(highscoreScore);
     }
 
     private void setupMainButton(){
